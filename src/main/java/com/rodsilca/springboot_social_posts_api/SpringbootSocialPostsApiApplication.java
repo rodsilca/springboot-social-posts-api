3af1996f287c6ce08f1252ad63a985ffebdc2570
rodsilca <cabralrodrigo787@gmail.com>
2025-08-01 16:18:30 -0400

    Project created

---
+++ src/main/java/com/rodsilca/springboot_social_posts_api/SpringbootSocialPostsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodsilca.springboot_social_posts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSocialPostsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSocialPostsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodsilca/springboot_social_posts_api/SpringbootSocialPostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodsilca.springboot_social_posts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSocialPostsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
