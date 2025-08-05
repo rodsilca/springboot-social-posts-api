7d41751cc8ab39fa6aab124390208279cd675bb2
rodsilca <cabralrodrigo787@gmail.com>
2025-08-05 12:05:16 -0400

    insert user

--- src/main/java/com/rodsilca/springboot_social_posts_api/resources/UserResource.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.rodsilca.springboot_social_posts_api.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.rodsilca.springboot_social_posts_api.domain.User;
 import com.rodsilca.springboot_social_posts_api.dto.UserDTO;
 import com.rodsilca.springboot_social_posts_api.services.UserService;
@@ -30,5 +35,13 @@ public class UserResource {
 		User user = service.fingById(id);
 		UserDTO userDTO = new UserDTO(user);
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@PutMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj){
+		User user = service.fromDTO(obj);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/rodsilca/springboot_social_posts_api/services/UserService.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodsilca.springboot_social_posts_api.domain.User;
+import com.rodsilca.springboot_social_posts_api.dto.UserDTO;
 import com.rodsilca.springboot_social_posts_api.repository.UserRepository;
 import com.rodsilca.springboot_social_posts_api.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User fingById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("User not found: "+id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
