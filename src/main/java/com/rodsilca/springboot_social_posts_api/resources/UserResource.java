e0bd0e6f20c8697e57f6572610133c5d610d7406
rodsilca <cabralrodrigo787@gmail.com>
2025-08-05 11:06:53 -0400

    Exceptions resource and service

--- src/main/java/com/rodsilca/springboot_social_posts_api/resources/UserResource.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.rodsilca.springboot_social_posts_api.domain.User;
@@ -22,5 +23,12 @@ public class UserResource {
 		List<User> usuarios = service.findAll();
 		List<UserDTO> listDto = usuarios.stream().map(x -> new UserDTO(x)).toList();
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.fingById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
 	}
 }
--- src/main/java/com/rodsilca/springboot_social_posts_api/services/UserService.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rodsilca.springboot_social_posts_api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodsilca.springboot_social_posts_api.domain.User;
 import com.rodsilca.springboot_social_posts_api.repository.UserRepository;
+import com.rodsilca.springboot_social_posts_api.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User fingById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("User not found: "+id));
 	}
 }
