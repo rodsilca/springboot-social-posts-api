27320cbf86385b90deb3b4cd70a44e167c68a679
rodsilca <cabralrodrigo787@gmail.com>
2025-08-11 14:48:03 -0400

    added comments to posts

--- src/main/java/com/rodsilca/springboot_social_posts_api/config/Instantiation.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rodsilca.springboot_social_posts_api.domain.Post;
 import com.rodsilca.springboot_social_posts_api.domain.User;
 import com.rodsilca.springboot_social_posts_api.dto.AuthorDTO;
+import com.rodsilca.springboot_social_posts_api.dto.CommentDTO;
 import com.rodsilca.springboot_social_posts_api.repository.PostRepository;
 import com.rodsilca.springboot_social_posts_api.repository.UserRepository;
 
@@ -43,9 +44,15 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2025"),"Viagem", "Vou viajar", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/03/2025"),"htklsfklsdf", "Vou kfjsdkfjsd",new AuthorDTO(maria) );
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem",sdf.parse("21/03/2025"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("21/03/2025"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um otimo dia",sdf.parse("21/03/2025"), new AuthorDTO(alex));
 		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
 		
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
--- src/main/java/com/rodsilca/springboot_social_posts_api/domain/Post.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/domain/Post.java
@@ -1,13 +1,16 @@
 package com.rodsilca.springboot_social_posts_api.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rodsilca.springboot_social_posts_api.dto.AuthorDTO;
+import com.rodsilca.springboot_social_posts_api.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -84,7 +89,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
 
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -104,7 +116,4 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-
-	
-	
 }
