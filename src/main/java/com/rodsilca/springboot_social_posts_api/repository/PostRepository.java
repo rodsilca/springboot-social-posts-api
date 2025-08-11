899c5050649fbc5f4f3e5b828081a8dc2d41fcf3
rodsilca <cabralrodrigo787@gmail.com>
2025-08-11 15:18:27 -0400

    Simple search w/ @Query

--- src/main/java/com/rodsilca/springboot_social_posts_api/repository/PostRepository.java
+++ src/main/java/com/rodsilca/springboot_social_posts_api/repository/PostRepository.java
@@ -3,9 +3,12 @@ package com.rodsilca.springboot_social_posts_api.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.rodsilca.springboot_social_posts_api.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 }
